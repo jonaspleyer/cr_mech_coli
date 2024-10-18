a41184915da9c1ec99ae4ddef8c7707482eefcff
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-10-18 14:42:52 +0200

    simplify basic test script; although not tests are performed here

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,47 +1,35 @@
 import cr_mech_coli as crm
-from tqdm import tqdm
+import numpy as np
+from concurrent.futures import ProcessPoolExecutor
 
-if __name__ == "__main__":
-    config = crm.Configuration()
-    config.show_progressbar = True
+def run_config(config):
+    config = crm.Configuration.from_json(config)
     config.n_agents = 8
     config.t0 = 0.0
     config.dt = 0.1
-    config.t_max = 120.0
-    config.save_interval = 10.0
-    config.agent_settings.mechanics.spring_tension = 0.2
-    config.agent_settings.mechanics.damping = 1.0
-    config.agent_settings.mechanics.angle_stiffness = 0.5
-    config.agent_settings.mechanics.spring_length = 3.0
-    config.agent_settings.interaction.strength = 0.1
-    config.agent_settings.interaction.potential_stiffness = 0.5
-    config.agent_settings.interaction.radius = 3.0
-    config.agent_settings.interaction.cutoff = 10.0
-    config.agent_settings.growth_rate = 20.0
-    config.agent_settings.spring_length_threshold = 6.0
+    config.t_max = 100.0
+    config.save_interval = 5.0
 
-    try:
-        cells = crm.run_simulation(config)
-        colors = crm.assign_colors_to_cells(cells)
-        iterations = sorted(cells.keys())
-        render_settings = crm.RenderSettings()
-        render_settings.noise = 50
-        render_settings.kernel_size = 30
-        render_settings.ssao_radius = 50
+    sim_result = crm.run_simulation(config)
+    render_settings = crm.RenderSettings()
+    render_settings.noise = 50
+    render_settings.kernel_size = 30
+    render_settings.ssao_radius = 50
 
-        for iteration in tqdm(iterations, total=len(iterations)):
-            crm.render_image(
-                config,
-                cells[iteration],
-                render_settings,
-                filename="out/images/snapshot_{:07}.png".format(iteration),
-            )
-            crm.render_mask(
-                config,
-                cells[iteration],
-                colors,
-                render_settings,
-                filename="out/masks/snapshot_{:07}-mask.png".format(iteration),
-            )
-    except Exception as error:
-        print(error)
+    crm.store_all_images(
+        config,
+        sim_result,
+        render_settings,
+        render_raw_pv=True,
+        render_mask_artistic=True,
+    )
+
+if __name__ == "__main__":
+    configs = [
+        crm.Configuration(growth_rate=rate).to_json()
+        for rate in np.linspace(0.0, 0.1, 20)
+    ]
+
+    pool = ProcessPoolExecutor(max_workers=14)
+
+    _ = pool.map(run_config, configs)
