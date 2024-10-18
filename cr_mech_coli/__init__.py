c7b7dfb0f2724233259878bf66cca0617efdd397
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-10-18 20:57:20 +0200

    add description of module and some documentation for common example

--- cr_mech_coli/__init__.py
+++ cr_mech_coli/__init__.py
@@ -1,7 +1,21 @@
-"""cr_mech_coli
-This module performs numerical simulations and visualizes their results.
-In the future, we aim to provide deep-learning techniques which are based in the images generated
-with this package.
+"""
+This package performs numerical simulations of rod-shaped bacterial cells.
+It can also visualize the results of these simulations and perform parameter estimations.
+
+Performing a simulation and storing the corresponding images is straightforward.
+All settings needed to run a single simulation are contained in the :class:`.Configuration` class.
+The routine is executed via the :func:`.run_simulation` function.
+
+>>> import cr_mech_coli as crm
+>>> config = crm.Configuration()
+>>> config.n_agents = 8
+>>> sim_result = crm.run_simulation(config)
+
+To adjust visualization settings, we provide the :class:`.RenderSettings` class.
+
+>>> render_settings = RenderSettings()
+>>> render_settings.noise = 30
+>>> crm.store_all_images(config, sim_result, save_dir="out")
 """
 
 from .simulation import *
