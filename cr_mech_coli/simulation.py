efc1a45749caf6ed2c3df3bf1e74faff894ee694
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-10-23 00:17:19 +0200

    remove function in favor of method on SimResult

--- cr_mech_coli/simulation.py
+++ cr_mech_coli/simulation.py
@@ -3,16 +3,3 @@ This module contains functionality to configure and run simulations.
 """
 
 from .cr_mech_coli_rs import run_simulation, AgentSettings, RodAgent, Configuration, sort_cellular_identifiers, CellIdentifier, SimResult
-
-def extract_all_identifiers(sim_result: dict) -> SimResult:
-    """Extracts all identifiers of the given simulation data and returns them as a set.
-
-    Args:
-        cells_at_iterations (dict):
-            A dictionary of which given an iteration returns the cells at this simulation step as
-            produced by the `run_simulation` function.
-
-    Returns:
-        set: All identifiers which were present during the simulation.
-    """
-    return set(sim_result.keys())
