e5db372c28cd1e25e7c17ae385f2ab48b5639c4a
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-10-23 00:11:55 +0200

    add stubs for new functions

--- cr_mech_coli/cr_mech_coli_rs.pyi
+++ cr_mech_coli/cr_mech_coli_rs.pyi
@@ -121,3 +121,60 @@ def sort_cellular_identifiers(identifiers: list[CellIdentifier]) -> list[CellIde
     """
     ...
 
+def counter_to_color(counter: int) -> list[int]:
+    """\
+    Converts an integer counter between 0 and 251^3-1 to an RGB value.
+    The reason why 251 was chosen is due to the fact that it is the highest prime number which is
+    below 255.
+    This will yield a Field of numbers Z/251Z.
+
+    To calculate artistic color values we multiply the counter by 157*163*173 which are three prime
+    numbers roughyl in the middle of 255.
+    The new numbers can be calculated via
+
+    >>> new_counter = counter * 157 * 163 * 173
+    >>> c1, mod = divmod(new_counter, 251**2)
+    >>> c2, mod = divmod(mod, 251)
+    >>> c3      = mod
+
+    Args:
+        counter (int): Counter between 0 and 251^3-1
+        artistic (bool): Enables artistic to provide larger differences between single steps instead
+            of simple incremental one.
+
+    Returns:
+        list[int]: A list with exactly 3 entries containing the calculated color."""
+    ...
+
+def color_to_counter(color: list[int]) -> int:
+    """\
+    Converts a given color back to the counter value.
+
+    The is the inverse of the :function:`counter_to_color` function.
+    The formulae can be calculated with the `Extended Euclidean Algorithm
+    <https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm>`_.
+    The multiplicative inverse (mod 251) of the numbers of 157, 163 and 173 are:
+
+    >>> assert (12590168 * 157) % 251**3 == 1
+    >>> assert (13775961 * 163) % 251**3 == 1
+    >>> assert (12157008 * 173) % 251**3 == 1
+
+    Thus the formula to calculate the counter from a given color is:
+
+    >>> counter = color[0] * 251**2 + color[1] * 251 + color[2]
+    >>> counter = (counter * 12590168) % 251**3
+    >>> counter = (counter * 13775961) % 251**3
+    >>> counter = (counter * 12157008) % 251**3
+    """
+    ...
+
+def assign_colors_to_cells(sim_result: SimResult) -> dict[CellIdentifier, list[int]]:
+    """\
+    This functions assigns unique colors to given cellular identifiers. Note that this
+
+    Args:
+        sim_result (dict): An instance of the :class:`SimResult` class.
+    Returns:
+        dict: A dictionary mapping :class:`CellIdentifier` to colors.
+    """
+    ...
