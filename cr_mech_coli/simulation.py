443639b04cd732cdc9b8c462cefe9c93da6e9975
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-10-23 01:32:54 +0200

    re-export RodMechanicsSettings and MorsePotentialF32

--- cr_mech_coli/simulation.py
+++ cr_mech_coli/simulation.py
@@ -2,4 +2,4 @@
 This module contains functionality to configure and run simulations.
 """
 
-from .cr_mech_coli_rs import run_simulation, AgentSettings, RodAgent, Configuration, sort_cellular_identifiers, CellIdentifier, SimResult
+from .cr_mech_coli_rs import run_simulation, AgentSettings, RodAgent, RodMechanicsSettings, Configuration, sort_cellular_identifiers, CellIdentifier, SimResult, MorsePotentialF32
