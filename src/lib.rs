f56af9200d167752181a83bddb986a2ea61378df
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-10-22 23:38:59 +0200

    add newly implement rust methods for color to counter conversion and back

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,12 @@
 //! The bacteria grow and divide thus resulting in a packed environment after short periods of
 //! time.
 
+mod imaging;
 mod sampling;
 mod simulation;
 
 pub use cellular_raza::prelude::{CellIdentifier, VoxelPlainIndex};
+pub use imaging::*;
 pub use sampling::*;
 pub use simulation::*;
 
@@ -20,6 +22,9 @@ fn cr_mech_coli_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sort_cellular_identifiers, m)?)?;
     m.add_class::<CellIdentifier>()?;
     m.add_class::<VoxelPlainIndex>()?;
+
+    m.add_function(wrap_pyfunction!(counter_to_color, m)?)?;
+    m.add_function(wrap_pyfunction!(color_to_counter, m)?)?;
     m.add_class::<Configuration>()?;
     m.add_class::<RodMechanicsSettings>()?;
     m.add_class::<AgentSettings>()?;
