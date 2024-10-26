8b7844b19cae9ae8237c350ee12b426109c8c7cf
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-10-26 15:52:53 +0200

    change function to return the complete difference image

--- src/fitting.rs
+++ src/fitting.rs
@@ -15,73 +15,76 @@ macro_rules! check_shape_identical_nonempty(
     };
 );
 
+macro_rules! new_error (
+    ($error_kind:ident, $($message:tt),*) => {
+        pyo3::exceptions:: $error_kind ::new_err(format!($($message),*))
+    };
+);
+
 /// Calculates the penalty in relative area difference accounting for a lower number when cells have
 /// the same parent.
 #[pyfunction]
-pub fn penalty_area_diff_account_parents<'py>(
+#[pyo3(signature = (mask1, mask2, cell_container))]
+pub fn parents_diff_mask<'py>(
+    py: Python<'py>,
     mask1: numpy::PyReadonlyArray3<'py, u8>,
     mask2: numpy::PyReadonlyArray3<'py, u8>,
     cell_container: &CellContainer,
-    colors: HashMap<CellIdentifier, [u8; 3]>,
-    penalty_with_parent: f32,
-) -> pyo3::PyResult<f32> {
+) -> pyo3::PyResult<Bound<'py, numpy::PyArray2<u8>>> {
     use numpy::*;
     let m1 = mask1.as_array();
     let m2 = mask2.as_array();
     check_shape_identical_nonempty!(m1, m2);
-    let new_shape = [m1.shape()[0] * m1.shape()[1], m1.shape()[2]];
+    let s = m1.shape();
+    let new_shape = [s[0] * s[1], s[2]];
     let m1 = m1
         .to_shape(new_shape)
-        .or_else(|e| Err(pyo3::exceptions::PyValueError::new_err(format!("{e}"))))?;
+        .or_else(|e| Err(new_error!(PyValueError, "{e}")))?;
     let m2 = m2
         .to_shape(new_shape)
-        .or_else(|e| Err(pyo3::exceptions::PyValueError::new_err(format!("{e}"))))?;
-    let mut color_to_parent_color = HashMap::<[u8; 3], [u8; 3]>::new();
-    let penalty: f32 = m1
-        .axis_iter(numpy::ndarray::Axis(0))
-        .zip(m2.axis_iter(numpy::ndarray::Axis(0)))
-        .map(|(c1, c2)| -> pyo3::PyResult<f32> {
-            let c1 = [c1[0], c1[1], c1[2]];
-            let c2 = [c2[0], c2[1], c2[2]];
-            // First check if this color combination has already been calculated
-            let mut get_parent = |color| -> pyo3::PyResult<Option<[u8; 3]>> {
-                match color_to_parent_color.get(&color) {
-                    Some(p) => Ok(Some(p.clone())),
-                    None => {
-                        let counter = color_to_counter(c1);
-                        let ident = cell_container.counter_to_cell_identifier(counter)?;
-                        let parent_ident = cell_container.get_parent(&ident)?;
-                        match parent_ident {
-                            Some(p) => {
-                                let color_parent = colors.get(&p);
-                                match color_parent {
-                                    Some(cp) => {
-                                        color_to_parent_color.insert(color, cp.clone());
-                                    }
-                                    None => (),
-                                }
-                                Ok(color_parent.copied())
-                            }
-                            None => Ok(None),
-                        }
+        .or_else(|e| Err(new_error!(PyValueError, "{e}")))?;
+    let diff_mask = numpy::ndarray::Array1::<u8>::from_iter(
+        m1.outer_iter()
+            .zip(m2.outer_iter())
+            .map(|(c1, c2)| {
+                if c1 != c2 && c1.sum() != 0 && c2.sum() != 0 {
+                    let c1 = [c1[0], c1[1], c1[2]];
+                    let c2 = [c2[0], c2[1], c2[2]];
+
+                    let i1 = cell_container.color_to_cell.get(&c1).ok_or(new_error!(
+                        PyKeyError,
+                        "could not find color {:?}",
+                        c1
+                    ))?;
+                    let i2 = cell_container.color_to_cell.get(&c2).ok_or(new_error!(
+                        PyKeyError,
+                        "could not find color {:?}",
+                        c2
+                    ))?;
+
+                    // Check if one is the parent of the other
+                    let p1 = cell_container.parent_map.get(&i1).ok_or(new_error!(
+                        PyKeyError,
+                        "could not find cell {:?}",
+                        i1
+                    ))?;
+                    let p2 = cell_container.parent_map.get(&i2).ok_or(new_error!(
+                        PyKeyError,
+                        "could not find cell {:?}",
+                        i2
+                    ))?;
+
+                    if Some(i1) == p2.as_ref() || Some(i2) == p1.as_ref() {
+                        return Ok(1);
                     }
                 }
-            };
-            let p1 = get_parent(c1)?;
-            let p2 = get_parent(c2)?;
-            if p1 == p2 {
-                println!("[x] got parent");
-                Ok(penalty_with_parent)
-            } else {
-                println!("[ ] no parent");
-                Ok(1.0)
-            }
-        })
-        .collect::<Result<Vec<f32>, _>>()?
-        .into_iter()
-        .sum();
-
-    let s = m1.shape();
-    let size = s[0] as u64 * s[1] as u64;
-    Ok(penalty as f32 / size as f32)
+                Ok(0)
+            })
+            .collect::<pyo3::PyResult<Vec<u8>>>()?
+            .into_iter(),
+    );
+    let diff_mask = diff_mask
+        .to_shape([s[0], s[1]])
+        .or_else(|e| Err(pyo3::exceptions::PyValueError::new_err(format!("{e}"))))?;
+    Ok(diff_mask.to_pyarray_bound(py))
 }
