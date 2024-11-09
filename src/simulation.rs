9b0a3ac00d1e973c2bca64e1c871f30fea0f0333
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-11-09 01:53:43 +0100

    move code to separate agent module

--- src/simulation.rs
+++ src/simulation.rs
@@ -8,8 +8,7 @@ use time::FixedStepsize;
 
 use crate::datatypes::CellContainer;
 
-/// Determines the number of subsections to use for each bacterial rod
-pub const N_ROD_SEGMENTS: usize = 8;
+use crate::agent::*;
 
 /// Contains all settings required to construct :class:`RodMechanics`
 #[pyclass]
@@ -289,101 +288,6 @@ mod test_config {
             let h2 = c2.borrow(py).to_hash().unwrap();
             assert!(h1 != h2);
         });
-    }
-}
-
-/// A basic cell-agent which makes use of
-/// `RodMechanics <https://cellular-raza.com/docs/cellular_raza_building_blocks/structs.RodMechanics.html>`_
-#[pyclass]
-#[derive(CellAgent, Clone, Debug, Deserialize, Serialize)]
-pub struct RodAgent {
-    /// Determines mechanical properties of the agent.
-    /// See :class:`RodMechanics`.
-    #[Mechanics]
-    pub mechanics: RodMechanics<f32, N_ROD_SEGMENTS, 3>,
-    /// Determines interaction between agents. See [MorsePotentialF32].
-    #[Interaction]
-    pub interaction: RodInteraction<MorsePotentialF32>,
-    /// Rate with which the cell grows in units `1/MIN`.
-    #[pyo3(set, get)]
-    pub growth_rate: f32,
-    /// Threshold at which the cell will divide in units `MICROMETRE`.
-    #[pyo3(set, get)]
-    pub spring_length_threshold: f32,
-}
-
-#[pymethods]
-impl RodAgent {
-    fn __repr__(&self) -> String {
-        format!("{:?}", self)
-    }
-
-    /// Position of the agent given by a matrix containing all vertices in order.
-    #[getter]
-    pub fn pos<'a>(&'a self, py: Python<'a>) -> Bound<'a, numpy::PyArray2<f32>> {
-        use numpy::ToPyArray;
-        let new_array = numpy::nalgebra::SMatrix::<f32, N_ROD_SEGMENTS, 3>::from_iterator(
-            self.mechanics.pos.iter().map(|&x| x),
-        );
-        new_array.to_pyarray_bound(py)
-    }
-
-    /// Position of the agent given by a matrix containing all vertices in order.
-    #[setter]
-    pub fn set_pos<'a>(&'a mut self, pos: Bound<'a, numpy::PyArray2<f32>>) -> pyo3::PyResult<()> {
-        use numpy::PyArrayMethods;
-        let iter: Vec<f32> = pos.to_vec()?;
-        self.mechanics.pos =
-            nalgebra::SMatrix::<f32, N_ROD_SEGMENTS, 3>::from_iterator(iter.into_iter());
-        Ok(())
-    }
-
-    /// Velocity of the agent given by a matrix containing all velocities at vertices in order.
-    #[getter]
-    pub fn vel<'a>(&'a self, py: Python<'a>) -> Bound<'a, numpy::PyArray2<f32>> {
-        use numpy::ToPyArray;
-        let new_array = numpy::nalgebra::SMatrix::<f32, N_ROD_SEGMENTS, 3>::from_iterator(
-            self.mechanics.vel.iter().map(|&x| x),
-        );
-        new_array.to_pyarray_bound(py)
-    }
-
-    /// Velocity of the agent given by a matrix containing all velocities at vertices in order.
-    #[setter]
-    pub fn set_vel<'a>(&'a mut self, pos: Bound<'a, numpy::PyArray2<f32>>) -> pyo3::PyResult<()> {
-        use numpy::PyArrayMethods;
-        let iter: Vec<f32> = pos.to_vec()?;
-        self.mechanics.vel =
-            nalgebra::SMatrix::<f32, N_ROD_SEGMENTS, 3>::from_iterator(iter.into_iter());
-        Ok(())
-    }
-
-    /// The interaction radius as given by the [MorsePotentialF32] interaction struct.
-    #[getter]
-    pub fn radius(&self) -> f32 {
-        self.interaction.0.radius
-    }
-}
-
-impl Cycle<RodAgent, f32> for RodAgent {
-    fn update_cycle(
-        _rng: &mut rand_chacha::ChaCha8Rng,
-        dt: &f32,
-        cell: &mut Self,
-    ) -> Option<CycleEvent> {
-        cell.mechanics.spring_length += cell.growth_rate * dt;
-        if cell.mechanics.spring_length > cell.spring_length_threshold {
-            Some(CycleEvent::Division)
-        } else {
-            None
-        }
-    }
-
-    fn divide(_rng: &mut rand_chacha::ChaCha8Rng, cell: &mut Self) -> Result<Self, DivisionError> {
-        let c2_mechanics = cell.mechanics.divide(cell.interaction.0.radius)?;
-        let mut c2 = cell.clone();
-        c2.mechanics = c2_mechanics;
-        Ok(c2)
     }
 }
 
