169205eab5057854aea2660b64972e7084836fc1
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2025-01-31 18:47:56 +0100

    restructure test to actually check for the number of generated images in output folder

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,37 +1,34 @@
 import cr_mech_coli as crm
-import numpy as np
-from concurrent.futures import ProcessPoolExecutor
+from glob import glob
 
 
-def run_config(config):
-    config = crm.Configuration.from_json(config)
+def test_store_iamges():
+    config = crm.Configuration()
     config.t0 = 0.0
     config.dt = 0.1
-    config.t_max = 100.0
+    config.t_max = 40.0
     config.save_interval = 20.0
+    config.n_agents = 5
 
     agent_settings = crm.AgentSettings()
+    agent_settings.growth_rate = 0.02
+
     cell_container = crm.run_simulation(config, agent_settings)
     render_settings = crm.RenderSettings()
     render_settings.noise = 50
     render_settings.kernel_size = 30
     render_settings.ssao_radius = 50
 
+    save_dir = "./tests/test_image_gen_basic/"
     crm.store_all_images(
         cell_container,
         config.domain_size,
         render_settings,
         render_raw_pv=True,
+        save_dir=save_dir,
     )
 
-
-if __name__ == "__main__":
-    configs = [
-        crm.Configuration(growth_rate=rate, n_agents=n_agents).to_json()
-        for rate in np.linspace(0.0, 0.1, 20)
-        for n_agents in np.arange(3, 6)
-    ]
-
-    pool = ProcessPoolExecutor(max_workers=14)
-
-    _ = pool.map(run_config, configs)
+    # Check that the files exist
+    assert len(glob(f"{save_dir}images/*.png")) == 2
+    assert len(glob(f"{save_dir}masks/*.png")) == 2
+    assert len(glob(f"{save_dir}raw_pv/*.png")) == 2
