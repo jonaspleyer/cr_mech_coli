3ac8d8a6d602e6e996a8414bbbd6bad44722d65b
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2025-02-12 01:19:18 +0100

    formatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,54 +8,54 @@
 
 import os
 import sys
-sys.path.append(os.path.abspath('../../'))
+
+sys.path.append(os.path.abspath("../../"))
 import cr_mech_coli
 
-project = 'cr_mech_coli'
-copyright = '2024, Jonas Pleyer'
-author = 'Jonas Pleyer'
-release = '0.2.0'
+project = "cr_mech_coli"
+copyright = "2024, Jonas Pleyer"
+author = "Jonas Pleyer"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'myst_parser',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.autosectionlabel',
-    'sphinxcontrib.bibtex',
-    'sphinx_subfigure',
+    "myst_parser",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.autosectionlabel",
+    "sphinxcontrib.bibtex",
+    "sphinx_subfigure",
 ]
 
 source_suffix = {
-    '.rst': 'restructuredtext',
-    '.txt': 'markdown',
-    '.md': 'markdown',
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
 }
 
 autosummary_generate = True
 add_module_names = False
-autodoc_typehints_format = 'short'
+autodoc_typehints_format = "short"
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 
-bibtex_bibfiles = ['references.bib']
-bibtex_default_style = 'unsrt'
-bibtex_reference_style = 'label'
+bibtex_bibfiles = ["references.bib"]
+bibtex_default_style = "unsrt"
+bibtex_reference_style = "label"
 
 html_logo = "_static/cr_mech_coli_dark_mode.png"
 html_theme_options = {
