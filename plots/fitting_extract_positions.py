7703dbdb35d189a49ef81458eae61645ac465869
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2025-02-14 02:42:05 +0100

    add arguments to optionally skip parts of the plotting

--- plots/fitting_extract_positions.py
+++ plots/fitting_extract_positions.py
@@ -46,6 +46,9 @@ if __name__ == "__main__":
         type=int,
         default=8,
     )
+    parser.add_argument("--skip-masks", action="store_true", default=False)
+    parser.add_argument("--skip-graph", action="store_true", default=False)
+    parser.add_argument("--skip-distribution", action="store_true", default=False)
     pyargs = parser.parse_args()
 
     n_vertices = pyargs.n_vertices
@@ -71,63 +74,70 @@ if __name__ == "__main__":
     iterations = cell_container.get_all_iterations()
     colors = cell_container.cell_to_color
 
-    # Pick one iteration to plot results
-    iter_masks = [
-        (
-            iterations[9],
-            crm.render_mask(all_cells[iterations[9]], colors, config.domain_size),
-        ),
-        (
-            iterations[19],
-            crm.render_mask(all_cells[iterations[18]], colors, config.domain_size),
-        ),
-        (
-            iterations[29],
-            crm.render_mask(all_cells[iterations[29]], colors, config.domain_size),
-        ),
-        (
-            iterations[39],
-            crm.render_mask(all_cells[iterations[39]], colors, config.domain_size),
-        ),
-        (
-            iterations[49],
-            crm.render_mask(all_cells[iterations[49]], colors, config.domain_size),
-        ),
-    ]
-    for iteration, mask in tqdm(iter_masks):
-        positions = crm.extract_positions(mask, n_vertices=n_vertices)
-        positions = np.round(np.array(positions))
-        positions = np.array(positions, dtype=int).reshape((len(positions), -1, 1, 2))
-
-        dl = 2**0.5 * config.domain_size
-        domain_pixels = np.array(mask.shape[:2], dtype=float)
-        pixel_per_length = domain_pixels / dl
-
-        # Calculate differences in positions
-        pos_exact = []
-        for p1 in positions:
-            # Get color
-            color = mask[p1[0][0][1], p1[0][0][0]]
-            ident = cell_container.get_cell_from_color([*color])
-            cell = all_cells[iteration][ident][0]
-            p1 = np.array(p1[:, 0, :], dtype=float)
-            p2 = crm.convert_cell_pos_to_pixels(
-                cell.pos, config.domain_size, mask.shape[:2]
+    if not pyargs.skip_masks:
+        # Pick one iteration to plot results
+        iter_masks = [
+            (
+                iterations[9],
+                crm.render_mask(all_cells[iterations[9]], colors, config.domain_size),
+            ),
+            (
+                iterations[19],
+                crm.render_mask(all_cells[iterations[18]], colors, config.domain_size),
+            ),
+            (
+                iterations[29],
+                crm.render_mask(all_cells[iterations[29]], colors, config.domain_size),
+            ),
+            (
+                iterations[39],
+                crm.render_mask(all_cells[iterations[39]], colors, config.domain_size),
+            ),
+            (
+                iterations[49],
+                crm.render_mask(all_cells[iterations[49]], colors, config.domain_size),
+            ),
+        ]
+        for iteration, mask in tqdm(iter_masks):
+            positions = crm.extract_positions(mask, n_vertices=n_vertices)
+            positions = np.round(np.array(positions))
+            positions = np.array(positions, dtype=int).reshape(
+                (len(positions), -1, 1, 2)
             )
-            pos_exact.append(p2)
 
-        pos_exact = np.round(np.array(pos_exact)).reshape((len(pos_exact), -1, 1, 2))
-        pos_exact = np.array(pos_exact, dtype=int)
-        # mask = cv.polylines(mask, positions, False, (50, 50, 50), 2)
-        mask = cv.polylines(mask, pos_exact, False, (150, 150, 150), 2)
-        mask = cv.polylines(mask, pos_exact, False, (250, 250, 250), 1)
-        for p in positions.reshape((-1, 2)):
-            mask = cv.drawMarker(mask, p, (50, 50, 50), cv.MARKER_TILTED_CROSS, 14, 2)
-        path = Path("docs/source/_static/fitting-methods/")
-        cv.imwrite(
-            filename=str(path / "extract_positions-{:06}.png".format(iteration)),
-            img=mask[200:-200, 200:-200],
-        )
+            dl = 2**0.5 * config.domain_size
+            domain_pixels = np.array(mask.shape[:2], dtype=float)
+            pixel_per_length = domain_pixels / dl
+
+            # Calculate differences in positions
+            pos_exact = []
+            for p1 in positions:
+                # Get color
+                color = mask[p1[0][0][1], p1[0][0][0]]
+                ident = cell_container.get_cell_from_color([*color])
+                cell = all_cells[iteration][ident][0]
+                p1 = np.array(p1[:, 0, :], dtype=float)
+                p2 = crm.convert_cell_pos_to_pixels(
+                    cell.pos, config.domain_size, mask.shape[:2]
+                )
+                pos_exact.append(p2)
+
+            pos_exact = np.round(np.array(pos_exact)).reshape(
+                (len(pos_exact), -1, 1, 2)
+            )
+            pos_exact = np.array(pos_exact, dtype=int)
+            # mask = cv.polylines(mask, positions, False, (50, 50, 50), 2)
+            mask = cv.polylines(mask, pos_exact, False, (150, 150, 150), 2)
+            mask = cv.polylines(mask, pos_exact, False, (250, 250, 250), 1)
+            for p in positions.reshape((-1, 2)):
+                mask = cv.drawMarker(
+                    mask, p, (50, 50, 50), cv.MARKER_TILTED_CROSS, 14, 2
+                )
+            path = Path("docs/source/_static/fitting-methods/")
+            cv.imwrite(
+                filename=str(path / "extract_positions-{:06}.png".format(iteration)),
+                img=mask[200:-200, 200:-200],
+            )
 
     arglist = [
         (all_cells[n_iter], cell_container, colors, config) for n_iter in iterations
@@ -137,36 +147,43 @@ if __name__ == "__main__":
     lengths1 = [r[1] for r in results]
     lengths2 = [r[2] for r in results]
 
-    fig, ax1 = plt.subplots()
+    if not pyargs.skip_graph:
+        fig, ax1 = plt.subplots()
 
-    x = np.arange(len(distances)) * config.save_interval
-    ax1.plot(
-        x,
-        [np.mean(li) for li in lengths1],
-        # yerr=[np.std(li) for li in lengths1],
-        linestyle="--",
-        color="k",
-        label="Average Rod Length (Fit)",
-    )
-    ax1.fill_between(
-        x,
-        y1=[np.mean(lengths1[i]) - np.mean(distances[i]) for i in range(len(lengths1))],
-        y2=[np.mean(lengths1[i]) + np.mean(distances[i]) for i in range(len(lengths1))],
-        alpha=0.3,
-        color="gray",
-        label="Average Vertex Difference",
-    )
-    ax1.plot(
-        x,
-        [np.mean(li) for li in lengths2],
-        # yerr=[np.std(li) for li in lengths2],
-        linestyle=":",
-        color="k",
-        label="Average Rod Length",
-    )
-    ax1.legend()
-    ax1.set_ylabel("Length [µm]")
-    ax1.set_xlabel("Time [min]")
-    fig.tight_layout()
-    fig.savefig("docs/source/_static/fitting-methods/displacement-calculations.png")
-    plt.show()
+        x = np.arange(len(distances)) * config.save_interval
+        ax1.plot(
+            x,
+            [np.mean(li) for li in lengths1],
+            # yerr=[np.std(li) for li in lengths1],
+            linestyle="--",
+            color="k",
+            label="Average Rod Length (Fit)",
+        )
+        ax1.fill_between(
+            x,
+            y1=[
+                np.mean(lengths1[i]) - np.mean(distances[i])
+                for i in range(len(lengths1))
+            ],
+            y2=[
+                np.mean(lengths1[i]) + np.mean(distances[i])
+                for i in range(len(lengths1))
+            ],
+            alpha=0.3,
+            color="gray",
+            label="Average Vertex Difference",
+        )
+        ax1.plot(
+            x,
+            [np.mean(li) for li in lengths2],
+            # yerr=[np.std(li) for li in lengths2],
+            linestyle=":",
+            color="k",
+            label="Average Rod Length",
+        )
+        ax1.legend()
+        ax1.set_ylabel("Length [µm]")
+        ax1.set_xlabel("Time [min]")
+        fig.tight_layout()
+        fig.savefig("docs/source/_static/fitting-methods/displacement-calculations.png")
+        plt.show()
