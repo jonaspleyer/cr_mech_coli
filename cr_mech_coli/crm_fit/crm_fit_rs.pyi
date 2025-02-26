0cf1007f63f3ea24296cc59b745116ecd6d6b74d
Jonas Pleyer <jonas.dev@pleyer.org>
2025-02-26 01:02:09 +0100

    rename stub file and extend it with basic needs

---
+++ cr_mech_coli/crm_fit/crm_fit_rs.pyi
@@ -0,0 +1,58 @@
+import enum
+
+import cr_mech_coli as crm
+
+class PotentialType(enum.Enum):
+    Mie = 0
+    Morse = 1
+
+class PotentialType_Mie:
+    """
+    Variant of the :class:`PotentialType`.
+    """
+
+    ...
+
+class PotentialType_Morse: ...
+
+class SampledFloat:
+    min: float
+    max: float
+    initial: float
+    individual: bool | None
+
+class Parameter(enum.Enum):
+    SampledFloat
+    Float = float
+
+class Parameters:
+    radius: Parameter
+    rigidity: Parameter
+    damping: Parameter
+    strength: Parameter
+    potential_type: PotentialType
+
+class Constants:
+    t_max: float
+    dt: float
+    domain_size: float
+    n_voxels: int
+    rng_seed: int
+    cutoff: float
+    pixel_per_micron: float
+    n_vertices: int
+
+class Optimization:
+    seed: int
+    tol: float
+    max_iter: int
+    pop_size: int
+
+class Settings:
+    parameters: Parameters
+    constants: Constants
+    optimization: Optimization
+
+    @staticmethod
+    def from_toml(filename: str) -> Settings: ...
+    def to_config(self, n_saves: int) -> crm.Configuration: ...
