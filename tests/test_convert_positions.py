9b9a99968c55ea5a370de482ba6bd5ecae691309
Jonas Pleyer <jonas.dev@pleyer.org>
2025-02-27 22:04:03 +0100

    extend tests for conversion

--- tests/test_convert_positions.py
+++ tests/test_convert_positions.py
@@ -48,3 +48,46 @@ def test_convert_non_square_domain_pixel_to_length_and_back():
     q = crm.convert_pixel_to_position(p, domain_size, image_resolution)
     r = crm.convert_cell_pos_to_pixels(q, domain_size, image_resolution)
     check_diff_pixel(p, r)
+
+
+def test_convert_non_square_domain_domain_identity():
+    domain_size = (100, 200)
+    image_resolution = (100, 200)
+    p = np.linspace([10.0, 10.0], [40.0, 40.0])
+    q = crm.convert_cell_pos_to_pixels(p, domain_size, image_resolution)
+    check_diff_float(p, q, domain_size, image_resolution)
+
+
+def test_convert_non_square_pixel_domain_identity():
+    domain_size = (450.0, 150.0)
+    image_resolution = (450, 150)
+    p = np.array(np.round(np.linspace([10, 15], [350, 200])), dtype=int)
+    q = crm.convert_pixel_to_position(p, domain_size, image_resolution)
+    check_diff_pixel(p, q)
+
+
+def test_convert_domain_pixel_slope():
+    domain_size = (300, 300)
+    image_resolution = (1000, 600)
+
+    # This will have slope 1 in domain units
+    p = np.linspace([0, 0], domain_size, 10, dtype=float)
+    q = crm.convert_cell_pos_to_pixels(p, domain_size, image_resolution)
+
+    # Calculate ratios of domain and image_resolution
+    ratio_domain = p / np.array(domain_size)
+    ratio_image = q / np.array(image_resolution)
+    check_diff_float(ratio_domain, ratio_image, domain_size, image_resolution)
+
+
+def test_convert_pixel_domain_slope():
+    domain_size = (10_000, 300_000)
+    image_resolution = (1200, 800)
+
+    # This will have slope -1
+    q = np.linspace([0, image_resolution[1]], [image_resolution[0], 0], 10, dtype=int)
+    p = crm.convert_pixel_to_position(q, domain_size, image_resolution)
+
+    ratio_domain = p / np.array(domain_size)
+    ratio_image = q / np.array(image_resolution)
+    check_diff_float(ratio_domain, ratio_image, domain_size, image_resolution)
