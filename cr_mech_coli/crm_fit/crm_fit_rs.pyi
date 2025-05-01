06abb543c668df5d4e7eae2a5f5cfb9822f380e6
Jonas Pleyer <jonas.dev@pleyer.org>
2025-05-02 00:57:51 +0200

    correct name

--- cr_mech_coli/crm_fit/crm_fit_rs.pyi
+++ cr_mech_coli/crm_fit/crm_fit_rs.pyi
@@ -55,14 +55,14 @@ class Optimization:
     pop_size: int
     recombination: float
 
-class Other:
+class Others:
     show_progressbar: bool
 
 class Settings:
     parameters: Parameters
     constants: Constants
     optimization: Optimization
-    other: Other
+    others: Others
 
     @staticmethod
     def from_toml(filename: str) -> Settings: ...
