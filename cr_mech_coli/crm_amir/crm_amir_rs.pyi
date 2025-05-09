46b98566d6d9e6c4711aad13cb993e44684769f8
Jonas Pleyer <jonas.dev@pleyer.org>
2025-05-09 19:50:22 +0200

    improve type hints

--- cr_mech_coli/crm_amir/crm_amir_rs.pyi
+++ cr_mech_coli/crm_amir/crm_amir_rs.pyi
@@ -5,4 +5,16 @@ class FixedRod:
     domain_size: float
     block_size: float
 
-def run_sim(): ...
+class Parameters:
+    domain_size: float
+    block_size: float
+    t_max: float
+    save_interval: float
+    dt: float
+    rod_length: float
+    rod_rigiditiy: float
+    n_vertices: int
+
+    def __new__(cls) -> Parameters: ...
+
+def run_sim(parameters: Parameters): ...
