76d620c9a804b618de715c7cffb4a129ffe28d83
Jonas Pleyer <jonas.dev@pleyer.org>
2025-05-09 11:12:35 +0200

    add new script for fitting to values obtain by (Amir et al 2014)

--- docs/source/scripts/index.rst
+++ docs/source/scripts/index.rst
@@ -5,6 +5,7 @@ Scripts
    :maxdepth: 2
    :titlesonly:
 
+    crm_amir <crm_amir>
     crm_fit <crm_fit>
     crm_gen_data <crm_gen_data>
     crm_multilayer <crm_multilayer>
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@
 mod agent;
 mod cell_container;
 mod config;
+mod crm_amir;
 mod crm_fit;
 mod crm_multilayer;
 mod fitting;
@@ -46,6 +47,12 @@ fn cr_mech_coli(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
         "cr_mech_coli.crm_multilayer.crm_multilayer_rs",
         submodule_multilayer,
     )?;
+
+    let submodule_fit_amir = crm_amir::crm_amir(py)?;
+    m.add_submodule(&submodule_fit_amir)?;
+    py.import("sys")?
+        .getattr("modules")?
+        .set_item("cr_mech_coli.crm_amir.crm_amir_rs", submodule_fit_amir)?;
 
     m.add_function(wrap_pyfunction!(generate_positions, m)?)?;
     m.add_function(wrap_pyfunction!(generate_agents, m)?)?;
