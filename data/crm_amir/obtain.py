411ba069fb302690dfebd45ff78d1cf9945bdfd1
Jonas Pleyer <jonas.dev@pleyer.org>
2025-05-12 19:00:55 +0200

    make script work by applying custom filtering rules
    - this still does not determine positions for all images but that should
      not really matter
    - only tested for elastic case so far

--- data/crm_amir/obtain.py
+++ data/crm_amir/obtain.py
@@ -6,7 +6,8 @@ import cv2 as cv
 import numpy as np
 import cr_mech_coli as crm
 import skimage as sk
-import sklearn as skl
+from tqdm import tqdm
+import multiprocessing as mp
 
 MOVIE1 = "https://www.pnas.org/doi/suppl/10.1073/pnas.1317497111/suppl_file/sm01.mov"
 MOVIE2 = "https://www.pnas.org/doi/suppl/10.1073/pnas.1317497111/suppl_file/sm02.mov"
@@ -42,54 +43,69 @@ def get_frames(path, filename):
     subprocess.Popen(cmd, text=True, shell=True)
 
 
-def extract_masks(path):
+def extract_mask(iteration, img, save_progression: bool = False):
+    img2 = np.copy(img)
+    filt1 = img2[:, :, 1] <= 150
+    img2[filt1] = [0, 0, 0]
+    filt2 = np.all(img2 >= np.array([180, 180, 180]), axis=2)
+    img2[filt2] = [0, 0, 0]
+
+    cutoff = int(img2.shape[1] / 3)
+    filt3 = np.linalg.norm(img2 - GREEN_COLOR, axis=2) >= 100
+    filt3[:, :cutoff] = True
+    img2[filt3] = [0, 0, 0]
+
+    img3 = np.copy(img2)
+    img3[filt3 == 0] = GREEN_COLOR.astype(np.uint8)
+
+    img_filt = sk.segmentation.expand_labels(img3, distance=20)
+
+    img3 = np.repeat(np.all(img_filt != [0, 0, 0], axis=2), 3).reshape(
+        img_filt.shape
+    ).astype(int) * GREEN_COLOR.astype(int)
+    img4 = np.copy(img3).astype(np.uint8)
+
+    try:
+        pos = crm.extract_positions(img4)[0][0]
+        p = pos[:, ::-1].reshape((-1, 1, 2))
+        img4 = cv.polylines(
+            np.copy(img),
+            [p.astype(int)],
+            isClosed=False,
+            color=(10, 10, 230),
+            thickness=2,
+        )
+        ret = pos
+    except ValueError as e:
+        print(e)
+        ret = None
+
+    if save_progression:
+        cv.imwrite(f"progression-{iteration:06}-1.png", img)
+        cv.imwrite(f"progression-{iteration:06}-2.png", img2)
+        cv.imwrite(f"progression-{iteration:06}-3.png", img3)
+        cv.imwrite(f"progression-{iteration:06}-4.png", img4)
+
+    return ret
+
+
+def extract_masks(path: Path, save_progressions: list[int] = []):
     files = sorted(glob(str(path / "frames/*")))
     imgs = [cv.imread(f) for f in files]
 
-    img = imgs[10]
-
-    img1 = np.array(img)
-    filt1 = img[:, :, 1] <= 150
-    img1[filt1] = [0, 0, 0]
-    filt2 = np.all(img1 >= np.array([180, 180, 180]), axis=2)
-    img1[filt2] = [0, 0, 0]
-
-    filt3 = np.linalg.norm(img1 - GREEN_COLOR, axis=2) >= 50
-    img1[filt3] = [0, 0, 0]
-
-    img_filt = sk.segmentation.expand_labels(img1, distance=10)
-
-    import matplotlib.pyplot as plt
-
-    fig, ax = plt.subplots(ncols=2, nrows=2, sharex=True, figsize=(6, 8))
-
-    ax[0, 0].imshow(img1)
-    ax[0, 1].imshow(img_filt)
-    img3 = np.all(img_filt != [0, 0, 0], axis=2)
-    ax[1, 0].imshow(img3)
-    seed = np.copy(img3)
-    seed[1:-1, 1:-1] = img3.max()
-    filled = sk.morphology.reconstruction(seed, img3, method="erosion")
-    ax[1, 1].imshow(filled)
-
-    for i in range(2):
-        for j in range(2):
-            ax[i, j].axis("off")
-
-    try:
-        positions = crm.extract_positions(img_filt)[0]
-        ax[0, 1].plot(positions[0, :, 1], positions[0, :, 0], color="red", linewidth=2)
-    except ValueError as e:
-        print("Could not extract positions")
-        print(e)
-
-    fig.tight_layout()
-    plt.show()
+    for img_file, img in tqdm(zip(files, imgs), total=len(imgs)):
+        it = Path(img_file).stem
+        it = int(it)
+        position = extract_mask(it, img, it in save_progressions)
+        if position is not None:
+            np.savetxt((path / "positions") / f"position-{it:06}.txt", position)
+        else:
+            print(f"[{it:06}] Could not extract positions")
 
 
 if __name__ == "__main__":
     for name, url in zip(NAMES, LINKS):
         path, filename = download_movie(url, name)
         # get_frames(path, filename)
-        extract_masks(path)
+        extract_masks(path, save_progressions=[32])
         exit()
