870fe2f88e009b9b52e1ad269b610d9a2b98088a
Jonas Pleyer <jonas.dev@pleyer.org>
2025-05-15 04:34:06 +0200

    restructure exports; do not glob-export scripts; add crm_amir as submodule

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,18 @@
 mod agent;
 mod cell_container;
 mod config;
-mod crm_amir;
-mod crm_fit;
-mod crm_multilayer;
 mod fitting;
 mod imaging;
 mod simulation;
+
+/// Fit data to publication by (Amir et. al. 2014)
+pub mod crm_amir;
+/// Estimate parameters with classical methods from data
+pub mod crm_estimate_params;
+/// Fit a mechanical model to microscopic images
+pub mod crm_fit;
+/// Predict Multilayer Behaviour
+pub mod crm_multilayer;
 
 pub use agent::*;
 pub use cell_container::*;
@@ -53,6 +59,13 @@ fn cr_mech_coli(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     py.import("sys")?
         .getattr("modules")?
         .set_item("cr_mech_coli.crm_amir.crm_amir_rs", submodule_fit_amir)?;
+
+    let submodule_fit_amir = crm_estimate_params::crm_estimate_params_rs(py)?;
+    m.add_submodule(&submodule_fit_amir)?;
+    py.import("sys")?.getattr("modules")?.set_item(
+        "cr_mech_coli.crm_estimate_params.crm_estimate_params_rs",
+        submodule_fit_amir,
+    )?;
 
     m.add_function(wrap_pyfunction!(generate_positions, m)?)?;
     m.add_function(wrap_pyfunction!(generate_agents, m)?)?;
