0da96d0c5c84f8339e8593d73e18118840bc625a
Jonas Pleyer <jonas.dev@pleyer.org>
2025-09-21 21:46:39 +0200

    remove function; was inserted into crm_amir script

--- data/crm_amir/obtain.py
+++ data/crm_amir/obtain.py
@@ -5,17 +5,13 @@ from glob import glob
 import cv2 as cv
 import numpy as np
 import cr_mech_coli as crm
-import skimage as sk
 from tqdm import tqdm
-import multiprocessing as mp
 
 MOVIE1 = "https://www.pnas.org/doi/suppl/10.1073/pnas.1317497111/suppl_file/sm01.mov"
 MOVIE2 = "https://www.pnas.org/doi/suppl/10.1073/pnas.1317497111/suppl_file/sm02.mov"
 
 LINKS = [MOVIE1, MOVIE2]
 NAMES = ["elastic", "plastic"]
-
-GREEN_COLOR = np.array([21.5 / 100, 86.6 / 100, 21.6 / 100]) * 255
 
 
 def download_movie(url, name):
@@ -41,52 +37,6 @@ def get_frames(path, filename):
     frame_dir.mkdir(exist_ok=True, parents=True)
     cmd = f"ffmpeg -loglevel quiet -i {path / filename} {frame_dir / '%06d.png'}"
     subprocess.Popen(cmd, text=True, shell=True)
-
-
-def extract_mask(iteration, img, save_progression: bool = False):
-    img2 = np.copy(img)
-    filt1 = img2[:, :, 1] <= 150
-    img2[filt1] = [0, 0, 0]
-    filt2 = np.all(img2 >= np.array([180, 180, 180]), axis=2)
-    img2[filt2] = [0, 0, 0]
-
-    cutoff = int(img2.shape[1] / 3)
-    filt3 = np.linalg.norm(img2 - GREEN_COLOR, axis=2) >= 100
-    filt3[:, :cutoff] = True
-    img2[filt3] = [0, 0, 0]
-
-    img3 = np.copy(img2)
-    img3[filt3 == 0] = GREEN_COLOR.astype(np.uint8)
-
-    img_filt = sk.segmentation.expand_labels(img3, distance=20)
-
-    img3 = np.repeat(np.all(img_filt != [0, 0, 0], axis=2), 3).reshape(
-        img_filt.shape
-    ).astype(int) * GREEN_COLOR.astype(int)
-    img4 = np.copy(img3).astype(np.uint8)
-
-    try:
-        pos = crm.extract_positions(img4)[0][0]
-        p = pos[:, ::-1].reshape((-1, 1, 2))
-        img4 = cv.polylines(
-            np.copy(img),
-            [p.astype(int)],
-            isClosed=False,
-            color=(10, 10, 230),
-            thickness=2,
-        )
-        ret = pos
-    except ValueError as e:
-        print(e)
-        ret = None
-
-    if save_progression:
-        cv.imwrite(f"progression-{iteration:06}-1.png", img)
-        cv.imwrite(f"progression-{iteration:06}-2.png", img2)
-        cv.imwrite(f"progression-{iteration:06}-3.png", img3)
-        cv.imwrite(f"progression-{iteration:06}-4.png", img4)
-
-    return ret
 
 
 def extract_masks(path: Path, save_progressions: list[int] = []):
