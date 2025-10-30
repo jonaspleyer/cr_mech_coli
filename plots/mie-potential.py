90b729f44cdb9b7f126f12789712813caea18f27
Jonas Pleyer <jonas.dev@pleyer.org>
2025-10-30 13:30:21 +0100

    fix depiction of mie potential; upadte figures

--- plots/mie-potential.py
+++ plots/mie-potential.py
@@ -13,12 +13,25 @@ def plot_mie_potential():
 
     def mie(t, n, m, bound, cutoff):
         strength = C(n, m) * ((sigma(1, n, m) / t) ** n - (sigma(1, n, m) / t) ** m)
-        return np.minimum(strength, bound) * (t <= cutoff)
+        ds = np.abs(
+            C(n, m)
+            / sigma(1, n, m)
+            * (
+                n * (sigma(1, n, m) / t) ** (n + 1)
+                - m * (sigma(1, n, m) / t) ** (m + 1)
+            )
+        )
+        co = t <= cutoff
+        n = np.argmin(co)
+        bo = ds <= bound
+        m = np.argmax(bo)
+        s = strength * bo + (1 - bo) * (bound * (t[m] - t) + strength[m])
+        return s * co + (1 - co) * strength[n], m
 
-    bound = 2.5
+    bound = 6.0
     cutoff = 3.0
 
-    x = np.linspace(0.2, 3.3, 100)
+    x = np.linspace(0.01, 3.3, 100)
 
     data = [
         (2, 1, ":"),
@@ -31,10 +44,24 @@ def plot_mie_potential():
     fig, ax = plt.subplots(figsize=(8, 8))
     crm.plotting.configure_ax(ax)
 
+    yfinmax = -10
     for en, em, ls in data:
-        y = mie(x, en, em, bound, cutoff)
-        ax.plot(x, y, label=f"n={en},m={em}", linestyle=ls, color=crm.plotting.COLOR3)
+        y, m_bound = mie(x, en, em, bound, cutoff)
+        yfinmax = max(yfinmax, y[-1])
+        ax.plot(
+            x[: m_bound + 1], y[: m_bound + 1], linestyle=ls, color=crm.plotting.COLOR2
+        )
+        ax.plot(
+            x[m_bound:],
+            y[m_bound:],
+            label=f"n={en},m={em}",
+            linestyle=ls,
+            color=crm.plotting.COLOR3,
+        )
 
+    ax.vlines(cutoff, -2.5, yfinmax, color=crm.plotting.COLOR5)
+
+    ax.set_xlim(0, 3.3)
     ax.set_ylim(-2.5, 3)
     ax.legend()
     ax.set_title("Mie Potential")
