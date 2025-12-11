9f2e07148de1eed16629d88efbd0c6192ae8e9c9
Jonas Pleyer <jonas.dev@pleyer.org>
2025-12-11 13:41:55 +0100

    add plotting of morse potential
    - slightly adjust variables

--- plots/mie-potential.py
+++ plots/mie-potential.py
@@ -1,12 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from cr_mech_coli import crm_fit
 
 if __name__ == "__main__":
     bound = 6.0
-    cutoff = 3.0
+    cutoff = 3.5
 
-    x = np.linspace(0.01, 3.3, 100)
+    x = np.linspace(0.01, 3.9, 200)
 
     data = [
         (2, 1, ":"),
@@ -21,13 +22,37 @@ if __name__ == "__main__":
         fig_ax = (f[0], f[1])
     fig, ax = fig_ax
 
-    ax.set_xlim(0, 3.3)
+    ax.set_xlim(0, np.max(x))
     ax.set_ylim(-2.5, 3)
     ax.legend()
     ax.set_title("Mie Potential")
 
     ax.set_xlabel("Distance [R$_1$+R$_2$]")
-    ax.set_ylabel("Interaction Strength [U$_0$]")
+    ax.set_ylabel("Interaction Strength [V$_0$]")
 
     fig.savefig("docs/source/_static/mie-potential-shapes.png")
     fig.savefig("docs/source/_static/mie-potential-shapes.pdf")
+
+    plt.close(fig)
+
+    data2 = [(1.0, ":"), (1.5, "-."), (3.0, "--")]
+    fig_ax = None
+    for sti, ls in data2:
+        label = f"ω={sti:3.1f}/(R$_1$+R$_2$)"
+        f = crm_fit.plot_morse_potential(x, 1, sti, 1, cutoff, fig_ax, ls, label=label)
+        fig_ax = (f[0], f[1])
+    fig, ax = fig_ax
+
+    ax.set_xlim(0, np.max(x))
+    xmin = 0
+    xmax = 5
+    dx = 0.05 * (xmax - xmin)
+    ax.set_ylim(xmin - dx, xmax + dx)
+    ax.legend()
+    ax.set_title("Morse Potential")
+
+    ax.set_xlabel("Distance [R$_1$+R$_2$]")
+    ax.set_ylabel("Interaction Strength [V$_0$]")
+
+    fig.savefig("docs/source/_static/morse-potential-shapes.png")
+    fig.savefig("docs/source/_static/morse-potential-shapes.pdf")
