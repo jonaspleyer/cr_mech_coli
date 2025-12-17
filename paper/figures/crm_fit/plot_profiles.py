bbe4e7af8d6cbfd52dbea55bd2e1c4adc1a0cd33
Jonas Pleyer <jonas.dev@pleyer.org>
2025-12-17 18:17:07 +0100

    rework plotting of combined profiles

--- paper/figures/crm_fit/plot_profiles.py
+++ paper/figures/crm_fit/plot_profiles.py
@@ -8,7 +8,15 @@ from cr_mech_coli import crm_fit
 
 
 def plot_potential_single(
-    n, name, short, units, path, ax, label, color=crm.plotting.COLOR3
+    n,
+    name,
+    short,
+    units,
+    path,
+    ax,
+    label,
+    color=crm.plotting.COLOR3,
+    linestyle="--",
 ):
     savename = name.strip().lower().replace(" ", "-")
     y = np.loadtxt(path / f"profiles/profile-{savename}")
@@ -28,9 +36,9 @@ def plot_potential_single(
         x = np.load(param_path)
     except:
         x = np.linspace(bound_lower, bound_upper, len(y))
-        np.save(x, param_path)
+        np.save(param_path, x)
 
-    crm_fit.plot_profile_from_data(
+    x, y = crm_fit.plot_profile_from_data(
         x,
         y,
         p_fixed,
@@ -43,146 +51,121 @@ def plot_potential_single(
         ls_color=color,
         fill=False,
         label=label,
+        linestyle=linestyle,
+        filter_thresh=16 if name == "Strength" else 16,
     )
 
-    return ax, x, (y - final_cost) / displacement_error**2
+    return ax, x, y  # , (y - final_cost) / displacement_error**2
 
 
-def plot_single_profile_combination(name, short, units, n1_p1_l1, n2_p2_l2, odir):
-    crm.plotting.set_mpl_rc_params()
-    fig, ax = plt.subplots(figsize=(8, 8))
-    crm.plotting.configure_ax(ax)
+def combine_values(xs, ys) -> tuple[list, list]:
+    xall = []
+    for xi in xs:
+        xall.extend(xi)
 
-    x1 = []
-    y1 = []
-    if type(n1_p1_l1) is tuple:
-        n1, p1, label1 = n1_p1_l1
-        res1 = plot_potential_single(
-            n1,
-            name,
-            short,
-            units,
-            p1,
-            ax,
-            label1,
-            color=crm.plotting.COLOR3,
+    xcombined = np.sort(np.unique(xall))
+
+    # def interpolate(x1, x2, y1, y2, xmiddle):
+    #     return (xmiddle - x1) / (x2 - x1) * (y2 - y1) + y1
+
+    def interpolate(x, y, xnew):
+        # calculate derivative at point xnew
+        if xnew < x[0] or xnew > x[-1]:
+            return np.inf
+        # if i == 0:
+        #     dx = (y[1] - y[0]) / (x[1] - x[0])
+        # elif i == len(x) - 1:
+        #     dx = (y[-1] - y[-2]) / (x[-1] - x[-2])
+        else:
+            i = np.sum(x <= xnew) - 1
+            dt = np.clip(np.abs(xnew - x[i]) / (x[i + 1] - x[i]), 0, 1)
+            return (1 - dt) * y[i] + dt * y[i + 1]
+            # dx = (y[i + 1] - y[i]) / (x[i + 1] - x[i])
+            # dx2 = (y[i] - y[i - 1]) / (x[i] - x[i - 1])
+            # return xnew + dt * dx
+
+    # Create new y values
+    yall = []
+    for x, y in zip(xs, ys):
+        ynew = []
+        for xi in xcombined:
+            (i_x,) = np.where(xi == x)
+            if len(i_x) == 0:
+                yi = interpolate(x, y, xi)
+                ynew.append(yi)
+            else:
+                ynew.append(y[i_x[0]])
+        yall.append(ynew)
+
+    yall = np.min(yall, axis=0)
+
+    return xcombined, yall
+
+
+def plot_all_profiles_combined(*args, odir, bounds={}):
+    settings = []
+    infos = []
+    for p, label, kwargs in args:
+        s = crm_fit.Settings.from_toml(p / "settings.toml")
+        settings.append(s)
+        infos.append((s.generate_optimization_infos(6), kwargs))
+
+    infos_combined = {}
+    for (p, label, _), (info, kwargs) in zip(args, infos):
+        for n, (name, short, units) in enumerate(info.parameter_infos):
+            if name in infos_combined:
+                infos_combined[name].append((n, p, label, name, short, units, kwargs))
+            else:
+                infos_combined[name] = [(n, p, label, name, short, units, kwargs)]
+
+    for inf in infos_combined.values():
+        crm.plotting.set_mpl_rc_params()
+        fig, ax = plt.subplots(figsize=(8, 8))
+        crm.plotting.configure_ax(ax)
+
+        xall = []
+        yall = []
+
+        name = ""
+        for n, p, label, name, short, units, kwargs in inf:
+            ax, xi, yi = plot_potential_single(
+                n,
+                name,
+                short,
+                units,
+                p,
+                ax,
+                label,
+                **kwargs,
+            )
+            xall.append(xi)
+            yall.append(yi)
+
+        ncol = max(2, round(len(inf) / 2))
+        ax.legend(
+            loc="upper center",
+            bbox_to_anchor=(0.5, 1.15) if len(inf) == 4 else (0.5, 1.125),
+            ncol=ncol,
+            frameon=False,
         )
-        x1 = res1[1]
-        y1 = res1[2]
 
-    x2 = []
-    y2 = []
-    if type(n2_p2_l2) is tuple:
-        n2, p2, label2 = n2_p2_l2
-        res2 = plot_potential_single(
-            n2,
-            name,
-            short,
-            units,
-            p2,
-            ax,
-            label2,
-            color=crm.plotting.COLOR5,
-        )
-        x2 = res2[1]
-        y2 = res2[2]
+        if name in bounds:
+            xmin, xmax = bounds[name]
+            dx = xmax - xmin
+        else:
+            xmin = np.min([np.min(xi) for xi in xall])
+            xmax = np.max([np.max(xi) for xi in xall])
+            dx = xmax - xmin
+        ax.set_xlim(xmin - 0.05 * dx, xmax + 0.05 * dx)
 
-    ax.legend(
-        loc="upper center",
-        bbox_to_anchor=(0.5, 1.10),
-        ncol=2,
-        frameon=False,
-    )
-    ax.set_title(name, pad=40.0)
+        x, y = combine_values(xall, yall)
+        crm_fit.fill_confidence_levels(x, y, ax)
 
-    if type(n2_p2_l2) is tuple and type(n1_p1_l1) is tuple:
-        x_unique = np.sort(np.unique([*x1, *x2]))
-        x = []
-        y = []
-        for xi in x_unique:
-            n1 = np.where(xi >= x1)[0][-1]
-            n2 = np.where(xi >= x2)[0][-1]
-
-            yi1 = y1[n1]
-            yi2 = y2[n2]
-
-            if yi1 < yi2 and xi in x1:
-                x.append(xi)
-                y.append(yi1)
-            if yi2 < yi1 and xi in x2:
-                x.append(xi)
-                y.append(yi2)
-
-        x = np.array(x)
-        y = np.array(y)
-    elif type(n1_p1_l1) is tuple:
-        x = np.array(x1)
-        y = np.array(y1)
-    else:
-        x = np.array(x2)
-        y = np.array(y2)
-
-    thresh_prev = 0
-    for i, q in enumerate([0.68, 0.90, 0.95]):
-        thresh = sp.stats.chi2.ppf(q, 1)
-        color = crm.plotting.COLOR3 if i % 2 == 0 else crm.plotting.COLOR5
-        filt = y <= thresh
-        lower = np.max(np.array([y, np.repeat(thresh_prev, len(y))]), axis=0)
-        ax.fill_between(
-            x,
-            lower,
-            np.repeat(thresh, len(lower)),
-            where=filt,
-            interpolate=True,
-            color=color,
-            alpha=0.3,
-        )
-        thresh_prev = thresh
-
-    odir.mkdir(parents=True, exist_ok=True)
-
-    savename = name.strip().lower().replace(" ", "-")
-    fig.savefig(odir / f"profile-{savename}.pdf")
-    fig.savefig(odir / f"profile-{savename}.png")
-    plt.close(fig)
-
-
-def plot_all_profiles_combined(p1, p2, label1, label2, odir):
-    settings1 = crm_fit.Settings.from_toml(p1 / "settings.toml")
-    settings2 = crm_fit.Settings.from_toml(p2 / "settings.toml")
-
-    infos1 = settings1.generate_optimization_infos(6)
-    infos2 = settings2.generate_optimization_infos(6)
-
-    infos_combined = []
-    for n1, (name, short, units) in enumerate(infos1.parameter_infos):
-        infos_combined.append(((n1, p1, label1), None, name, short, units))
-    for n2, (name, short, units) in enumerate(infos2.parameter_infos):
-        found = False
-        for n, info in enumerate(infos_combined):
-            if name == info[2]:
-                # print(n, n2, name, info[2])
-                infos_combined[n] = (
-                    info[0],
-                    (n2, p2, label2),
-                    info[2],
-                    info[3],
-                    info[4],
-                )
-                found = True
-        if found is False:
-            pass
-            # infos_combined.append(((None, (n2, p2), (name, short, units))))
-
-    for n1_p1_l1, n2_p2_l2, name, short, units in infos_combined:
-        plot_single_profile_combination(
-            name,
-            short,
-            units,
-            n1_p1_l1,
-            n2_p2_l2,
-            odir,
-        )
+        ax.set_title(name, pad=55.0)
+        odir.mkdir(parents=True, exist_ok=True)
+        savename = name.strip().replace(" ", "-").lower()
+        fig.savefig(odir / f"profile-{savename}.png")
+        fig.savefig(odir / f"profile-{savename}.pdf")
 
 
 if __name__ == "__main__":
@@ -191,18 +174,16 @@ if __name__ == "__main__":
     path_morse_all = Path("figures/crm_fit/morse_all")
     path_morse_partial = Path("figures/crm_fit/morse_partial")
 
-    plot_all_profiles_combined(
-        path_mie_all,
-        path_mie_partial,
-        "full",
-        "λ=1.0 min$^{-1}$",
-        Path("figures/crm_fit/mie_profiles"),
-    )
+    bounds = {
+        "Strength": (0, 0.12),
+        "Damping": (0, 10.0),
+    }
 
     plot_all_profiles_combined(
-        path_morse_all,
-        path_morse_partial,
-        "full",
-        "λ=1.0 min$^{-1}$",
-        Path("figures/crm_fit/morse_profiles"),
+        (path_morse_all, "Morse", {"linestyle": "--", "color": crm.COLOR2}),
+        (path_morse_partial, "Morse λ=λ$_0$", {"linestyle": "-", "color": crm.COLOR3}),
+        (path_mie_all, "Mie", {"linestyle": "--", "color": crm.COLOR5}),
+        (path_mie_partial, "Mie λ=λ$_0$", {"linestyle": "-", "color": crm.COLOR6}),
+        odir=Path("figures/crm_fit/profiles"),
+        bounds=bounds,
     )
