416755a524a9517bfd195071c64e79e5f2be1e3d
Jonas Pleyer <jonas.dev@pleyer.org>
2025-12-29 00:13:07 +0100

    add tests to check that ml_configs are created correctly

--- tests/multilayer1.py
+++ tests/multilayer1.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import cr_mech_coli as crm
 import cr_mech_coli.crm_multilayer as crmm
 
@@ -29,3 +31,38 @@ def test_produce_ydata():
     assert ymax.shape == (len(iterations),)
     assert y95th.shape == (len(iterations),)
     assert ymean.shape == (len(iterations),)
+
+
+def set_strength(ml_config, x):
+    ml_config.agent_settings.interaction.strength = x
+
+
+def test_sample_parameters():
+    ml_configs = crmm.sample_parameters(
+        ("agent_settings.interaction.strength", 0.0, 10.0, 3),
+        ("agent_settings.mechanics.damping", 1.0, 5.0, 5),
+        ("agent_settings.mechanics.rigidity", 1.0, 10.0, 4, "log"),
+        ("config.gel_pressure", 0.0, 0.1, 7),
+    )
+    ml_configs = list(ml_configs)
+
+    # Check length of generated configs
+    assert len(ml_configs) == 3 * 5 * 4 * 7
+
+    for ml_config in ml_configs:
+        x = ml_config.agent_settings.interaction.strength
+        assert x == 0.0 or x == 5.0 or x == 10.0
+        y = ml_config.agent_settings.mechanics.damping
+        assert y == 1.0 or y == 2.0 or y == 3.0 or y == 4.0 or y == 5.0
+        z = ml_config.agent_settings.mechanics.rigidity
+        x0 = np.log(1)
+        x1 = np.log(10)
+        dx = (x1 - x0) / 3
+        assert (
+            z == 1.0
+            or np.abs(z - np.exp(x0 + dx)) < 1e-4
+            or np.abs(z - np.exp(x0 + 2 * dx)) < 1e-4
+            or np.abs(z - 10.0) < 1e-4
+        )
+        h = ml_config.config.gel_pressure
+        assert np.abs(h * 6 - np.round(h * 6, 1)) < 1e-6
