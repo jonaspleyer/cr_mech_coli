354c0808881e4ed61a2557a55af728579960102a
Jonas Pleyer <jonas.dev@pleyer.org>
2026-01-05 03:01:22 +0100

    add 2 new tests for plotters mask function

--- tests/test_fit.py
+++ tests/test_fit.py
@@ -117,3 +117,105 @@ def test_overlap():
 
     assert parent_penalty > o1
     assert parent_penalty > o2
+
+
+def test_render_mask_2d():
+    _, _, container = produce_masks()
+    config = __create_config()
+    i1 = container.get_all_iterations()[1]
+
+    resolution = (int(12.8 * config.domain_size[0]), int(12.8 * config.domain_size[1]))
+
+    import time
+
+    times = []
+    for _ in range(5):
+        t = time.process_time()
+        mask1, _overlap_mask = crm.render_mask_2d(
+            container.get_cells_at_iteration(i1),
+            container.cell_to_color,
+            (config.domain_size[0], config.domain_size[1]),
+            resolution,
+            delta_angle=np.float32(np.pi / 15.0),
+        )
+        t1 = time.process_time() - t
+        t = time.process_time()
+
+        mask2 = crm.render_mask(
+            container.get_cells_at_iteration(i1),
+            container.cell_to_color,
+            (config.domain_size[0], config.domain_size[1]),
+        )
+        t2 = time.process_time() - t
+
+        times.append([t1, t2])
+
+        _ = crm.parents_diff_mask(
+            mask1,
+            mask2,
+            container.color_to_cell,
+            container.parent_map,
+            0.5,
+        )
+
+        n1 = np.unique(mask1.reshape((-1, 3)), axis=0)
+        n2 = np.unique(mask2.reshape((-1, 3)), axis=0)
+        assert np.all(n1 == n2)
+
+    means = np.mean(times, axis=0)
+    std = np.std(times, axis=0)
+    # Assert that method is at least 10 times faster with 1sigma probability
+    t_pyvista = means[1] - std[1]
+    t_plotters = means[0] + std[0]
+    assert t_plotters * 10 < t_pyvista
+
+
+def test_render_mask_2d_2():
+    # This test originated from a bug that was found within the calculate_polygon_hull method
+    # where too many spherical coordinates were inserted in the middle of a rod
+    config = crm.Configuration()
+    agent_settings = crm.AgentSettings(
+        growth_rate=0.078947365,
+    )
+    config.t0 = 0.0
+    config.dt = 0.1
+    config.t_max = 200.0
+    config.n_saves = 10 - 2
+    config.domain_size = (np.float32(150.0), np.float32(150.0))
+
+    positions = crm.generate_positions(
+        n_agents=4,
+        agent_settings=agent_settings,
+        config=config,
+        rng_seed=1,
+        dx=(config.domain_size[0] * 0.1, config.domain_size[1] * 0.1),
+    )
+    agent_dict = agent_settings.to_rod_agent_dict()
+    cells = [crm.RodAgent(p, 0.0 * p, **agent_dict) for p in positions]
+
+    container = crm.run_simulation_with_agents(config, cells)
+
+    i1 = container.get_all_iterations()[2]
+    k, v = list(container.get_cells_at_iteration(i1).items())[1]
+    cells = {k: v}
+
+    m1, m2 = crm.render_mask_2d(
+        cells,
+        container.cell_to_color,
+        (config.domain_size[0], config.domain_size[1]),
+        (int(config.domain_size[0] * 10), int(config.domain_size[1] * 10)),
+        delta_angle=np.float32(np.pi / 15.0),
+    )
+
+    # Perform very crude thinning of non-zero values
+    filt = np.any(m1 != 0, axis=2)
+    filtnew = np.copy(filt)
+    filtnew[2:] *= filt[:-2]
+    filtnew[:-2] *= filt[2:]
+    filtnew[:, 2:] *= filt[:, :-2]
+    filtnew[:, :-2] *= filt[:, 2:]
+
+    import cv2 as cv
+
+    n_components, _ = cv.connectedComponents(filtnew.astype(np.uint8))
+    assert n_components >= 3
